6c2e04bcb3c1bd71a043c2ed6f1b36be72675594
shahbajjamil <shahbajjamil2014@gmail.com>
2024-12-09 15:27:50 +0530

    contact and skill added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
     <>
       <Hero />
       <Projects />
+      <Skills />
+      <Contact />
     </>
   );
 }
